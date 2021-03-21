c8e48f04b11cace0fc03d73b203fac94d4d96a49
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-03-21 13:10:09 +0200

    onClick added to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-const Button = ({color, text}) => {
-    return <button style = {{backgroundColor: color}} className="btn">{text}</button>
+const Button = ({color, text, onClick}) => {
+    return <button onClick={onClick} style={{backgroundColor: color}} className="btn">{text}</button>
 }
 
 Button.defaultProps = {
@@ -10,5 +10,6 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 export default Button;
