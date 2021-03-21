884eff6c373238fce6821ba847cc44737ae099ce
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-03-21 19:32:51 +0200

    adding footer and routing to About page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react'
+import { FaEarlybirds } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import { FaEarlybirds } from 'react-icons/fa';
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -83,14 +87,24 @@ function App() {
     )
   }
 
-return (
-  <div className="container">
-    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-    {showAddTask && <AddTask onAdd={addTask} />}
-    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
-      {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Display'}
-  </div>
-);
+  return (
+    <Router>
+
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
+              {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Display'}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
